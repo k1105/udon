8bd81349d218c060226442db33dbdff113de4119
k1105 <kanapon1105@yahoo.co.jp>
2022-01-18 00:29:06 +0900

    styled-cssの追加 #12

--- src/About.js
+++ src/About.js
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import Color from "./const/Color";
+
 const styles = {
   title: {
     color: "#EBDFD0",
@@ -19,8 +22,16 @@ const styles = {
 
 export const About = () => {
   return (
-    <div>
-      <p>hoge</p>
+    <div css="margin-top:100px">
+      <ContentText>
+        知人の依頼で、遺伝（いでん）的アルゴリズムを用いた形状自動生成のシステムを作っている傍ら息抜きに始めた。
+        ベジェ曲線でベジェベジェやってみたかった 目的:
+        experimentalな意味合いが強いので、ひとつ一つに明確な意義、目的は設定されていない。今後のためのアセット。
+      </ContentText>
     </div>
   );
 };
+
+const ContentText = styled.p`
+  color: ${Color.WHITE};
+`;
