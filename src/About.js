a29433aa0ae7ee8805e8a2184fd5888e234f18e8
k1105 <kanapon1105@yahoo.co.jp>
2022-01-17 21:38:47 +0900

    Aboutページへのページ遷移実装 #10

---
+++ src/About.js
@@ -0,0 +1,26 @@
+const styles = {
+  title: {
+    color: "#EBDFD0",
+    marginTop: "30px",
+    marginBottom: "10px",
+  },
+  credit: {
+    color: "#EBDFD0",
+    fontSize: "0.5rem",
+    textAlign: "center",
+    margin: "10px 0",
+  },
+  link: {
+    display: "inline-block",
+    marginBottom: "5px",
+    fontSize: "0.9rem",
+  },
+};
+
+export const About = () => {
+  return (
+    <div>
+      <p>hoge</p>
+    </div>
+  );
+};
